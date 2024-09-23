e76f81ca1d0c9407ba61afc7652a61d460d8ab06
MatheusCorreaaa <mcorreia073@gmail.com>
2024-09-23 13:23:14 -0300

    commit lista

---
+++ src/app/animation.ts
@@ -0,0 +1,16 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+export const highlightStateTrigger = trigger('highlightedState',[state('default',style({
+  border:'2px solid #B2B6FF'
+})),
+state('highlighted',style({
+  border:' 3px solid #B2B6FF',
+  filter:'brightness(92%)'
+})),
+transition('default => highlighted',[
+animate('200ms ease-out',style({
+  transform:'scale(1.02)'
+})),
+animate(200)
+])
+])
