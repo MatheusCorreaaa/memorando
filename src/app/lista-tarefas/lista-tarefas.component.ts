9ca4a8f1a3656716bb3f239388f204b61cdac87a
MatheusCorreaaa <mcorreia073@gmail.com>
2024-09-27 16:15:34 -0300

    animações

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,14 +4,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightStateTrigger } from '../animation';
+import { checkButtonTrigger, highlightStateTrigger, shownStateTrigger } from '../animation';
 
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations:[highlightStateTrigger]
+  animations:[highlightStateTrigger,shownStateTrigger,checkButtonTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
@@ -19,6 +19,7 @@ export class ListaTarefasComponent implements OnInit {
   categoria: string = '';
   validado: boolean = false;
   indexTarefas = -1;
+  id:number = 0;
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -111,6 +112,7 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   finalizarTarefa(id: number) {
+    this.id = id
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
         this.listarAposCheck();
